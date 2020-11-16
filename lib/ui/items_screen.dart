47ebd088e2283729c94a16dcbdc4d7147b2b6aba
tresbon <vladi.zubkov@gmail.com>
2020-11-16 18:27:38 +0300

    Added item edit dialog

--- lib/ui/items_screen.dart
+++ lib/ui/items_screen.dart
@@ -2,19 +2,31 @@ import 'package:flutter/material.dart';
 import '../dbhelper.dart';
 import '../models/list_items.dart';
 import '../models/shopping_list.dart';
+import 'list_item_dialog.dart';
 
 class ItemsScreen extends StatefulWidget {
   final ShoppingList shoppingList;
+
   ItemsScreen(this.shoppingList);
+
   @override
   _ItemsScreenState createState() => _ItemsScreenState(this.shoppingList);
 }
 
 class _ItemsScreenState extends State<ItemsScreen> {
   final ShoppingList shoppingList;
+
   _ItemsScreenState(this.shoppingList);
+
   DbHelper helper;
   List<ListItem> items;
+  ItemDialog dialog;
+
+  @override
+  void initState() {
+    dialog = ItemDialog();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -34,12 +46,16 @@ class _ItemsScreenState extends State<ItemsScreen> {
                 onTap: () {},
                 trailing: IconButton(
                   icon: Icon(Icons.edit),
-                  onPressed: () {},
+                  onPressed: () {
+                    showDialog(
+                      context: context,
+                      builder: (BuildContext context) =>
+                          dialog.buildDialog(context, items[index], false),
+                    );
+                  },
                 ),
               );
-            }
-        )
-    );
+            }));
   }
 
   Future showData(int idList) async {
